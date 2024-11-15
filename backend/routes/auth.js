f02bb63cfd9d21c658c25a3a233f7d67c6a189e5
Meshv12 <meshvpatel03940@gamil.com>
2024-11-15 16:18:15 +0530

    add login functionality

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { getAccount } = require("../controllers/auth/login");
 const { createAccount } = require("../controllers/auth/register");
 const { googleLogin, googleCredentials } = require("../controllers/auth/google");
 const { isEmailExists, isUsernameExists, isGoogleAuthenticated } = require("../controllers/auth/verifyCredentials");
@@ -8,6 +9,7 @@ const { resetPassword } = require("../controllers/auth/forgotPassword");
 const router = Router();
 
 router.post("/register", createAccount);
+router.post("/login", getAccount);
 router.get("/google-credentials", googleCredentials);
 router.post("/google-login", googleLogin);
 router.get("/verify-email", isEmailExists);
